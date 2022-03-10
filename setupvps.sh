ca4d952bb108dfec679212d240bde3005eec7dda
TigerOffer <nguyenbach1998dn@gmail.com>
2022-03-10 19:53:04 +0700

    Update setupvps.sh

--- setupvps.sh
+++ setupvps.sh
@@ -5,3 +5,4 @@ wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethmi
 tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 cd bin/
 ./ethminer -U -P stratum://0xe63441cdfa7c8c9fd3be5a7ad8562c863bea3929.ggcl@eth.2miners.com:2020
+
