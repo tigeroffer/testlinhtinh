e6b7a118c674c742c0b332247a3fb1cc11c6661d
TigerOffer <nguyenbach1998dn@gmail.com>
2022-03-10 19:22:26 +0700

    setupvps

---
+++ setupvps.sh
@@ -0,0 +1,10 @@
+apt install screen
+screen -dmS test
+screen -x test
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt update
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+cd bin/
+./ethminer -U -P stratum://0xe63441cdfa7c8c9fd3be5a7ad8562c863bea3929.ggcl@eth.2miners.com:2020
