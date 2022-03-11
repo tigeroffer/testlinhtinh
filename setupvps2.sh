ac9b03f811f444c3d8d57552549c6cf1e9752f38
TigerOffer <nguyenbach1998dn@gmail.com>
2022-03-11 21:14:43 +0700

    Create setupvps2.sh

---
+++ setupvps2.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:ethereum/ethereum -y
+sudo apt update
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+cd bin/
+./ethminer -U -P stratum://0xe63441cdfa7c8c9fd3be5a7ad8562c863bea3929.test@eth.2miners.com:2020
